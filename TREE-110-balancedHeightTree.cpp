753f12e4c1a938ddae6c5060e6843c247be4343d
Abrar Shahriar Mahtab <abrar.hit.edu.cn@gmail.com>
2026-06-06 03:09:30 +0800

    Update TREE-110-balancedHeightTree.cpp

--- TREE-110-balancedHeightTree.cpp
+++ TREE-110-balancedHeightTree.cpp
@@ -1,5 +1,6 @@
 // LeetCode 110: Balanced Binary Tree (Easy)Given a binary tree, determine if it is height-balanced.  
-//   A height-balanced binary tree is defined as a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+//   A height-balanced binary tree is defined as a binary tree in which the left
+//and right subtrees of every node differ in height by no more than 1.
 #include <algorithm> 
 using namespace std;
 
