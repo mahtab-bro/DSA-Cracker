2dd6c9c26debe696af266f3adfdae46776f5010f
Abrar Shahriar Mahtab <abrar.hit.edu.cn@gmail.com>
2026-06-05 21:17:52 +0800

    Update TREE-701-insertIntoBST.cpp

--- TREE-701-insertIntoBST.cpp
+++ TREE-701-insertIntoBST.cpp
@@ -1,7 +1,7 @@
 // LeetCode 701: Insert into a Binary Search Tree
 // Problem Description
-// You are given the root node of a binary search tree (BST) and a val to insert into the tree. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.  
-
+// You are given the root node of a binary search tree (BST) and a val to insert into the tree. Return the root node of the BST after the insertion. 
+//It is guaranteed that the new value does not exist in the original BST.  
 // Notice that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
 
 class Solution {
